59321400f09f826a6a6c4760ba4cb5f6b8a73123
Matias Leon <matiasleon2506@gmail.com>
2023-04-23 21:30:11 -0300

    agrego metodos a lso demas input, falta agregar evento al button al enviar y medias queries

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
-
+let form = document.getElementById ("form")
 
 class Card{
     constructor(){
 
         //Nombre de tarjeta
-        this.fullname = document.getElementById ("fullname")
-        this.changeFullName = document.getElementById ("changeFullName")
-        this.errorName = document.getElementById("errorName")
+        this.fullname = document.getElementById ("fullname");
+        this.changeFullName = document.getElementById ("changeFullName");
+        this.errorName = document.getElementById("errorName");
 
         //Numero de tarjeta
-        this.numberCard = document.getElementById ("numberCard")
-        this.errorNumber = document.getElementById ("errorNumber")
-        this.changeNumberCard = document.getElementById ("changeNumberCard")
+        this.numberCard = document.getElementById ("numberCard");
+        this.errorNumber = document.getElementById ("errorNumber");
+        this.changeNumberCard = document.getElementById ("changeNumberCard");
+
+        //Fecha expiración
+        this.dateMM = document.getElementById ("dateMM");
+        this.changeMM = document.getElementById ("changeMM");
+        this.dateYY = document.getElementById ("dateYY");
+        this.changeYY = document.getElementById ("changeYY");
+        this.errorFecha = document.getElementById ("errorFecha");
+
+        //CVC
+        this.cvc = document.getElementById ("cvc");
+        this.errorCVC = document.getElementById ("errorCVC");
+        this.changeCVC = document.getElementById ("changeCVC")
+
+
+        //BUTTON
+        this.button = document.getElementById ("button")
     }
 
 
     agregarNombre(){
+        this.fullname.nodeValue
         this.fullname.addEventListener("input", ()=>{
             this.changeFullName.innerHTML = this.fullname.value.toUpperCase()
             this.verificarNombre()
@@ -27,8 +44,11 @@ class Card{
         if(!validation.test(this.fullname.value)){
             this.errorName.innerHTML = "Currently Error"
         }else this.errorName.innerHTML = ""
+        return true
 
     }
+
+
 
     agregarNumero(){
         this.numberCard.addEventListener("input", ()=>{
@@ -44,6 +64,67 @@ class Card{
         }else this.errorNumber.innerHTML = ""
     }
 
+
+
+    agregarFechaMM(){
+        this.dateMM.addEventListener ("input", ()=>{
+            this.changeMM.innerHTML = this.dateMM.value + "/"
+            this.verificarMM()
+        })
+    }
+
+    verificarMM(){
+        let validar = new RegExp ('^[0-9 ]+$', 'i');
+        if (!validar.test(this.dateMM.value) || (this.dateMM.value <= 0 || this.dateMM.value >12)){
+            this.errorFecha.innerHTML = "Currently Error"
+        }else this.errorFecha.innerHTML = ""
+    }
+
+
+
+    agregarFechaYY(){
+        this.dateYY.addEventListener ("input", ()=>{
+            this.changeYY.innerHTML = this.dateYY.value
+            this.verificarYY()
+        })
+    }
+
+    verificarYY(){
+        let validar = new RegExp ('^[0-9 ]+$', 'i');
+        if (!validar.test(this.dateYY.value) || (this.dateYY.value <23 || this.dateYY.value > 30) ){
+            this.errorFecha.innerHTML = "Currently Error"
+            return form = false
+        }else this.errorFecha.innerHTML = ""
+        return form = true
+    }
+
+
+
+    agregarCVC(){
+        this.cvc.addEventListener ("input", ()=>{
+            this.changeCVC.innerHTML = this.cvc.value
+            this.verificarCVC();
+        })
+    }
+
+    verificarCVC(){
+        let validar = new RegExp ('^[0-9 ]+$', 'i');
+        if (!validar.test(this.cvc.value)){
+            this.errorCVC.innerHTML = "Currently Error"
+            return form = false
+        }else this.errorCVC.innerHTML = ""
+        return form
+    }
+
+    formEnviado(){
+        this.button.addEventListener ("click", ()=>{
+            if (this.fullname.value != null && this.numberCard.value != 0 && this.cvc.value != 0 && this.dateMM.value != 0){
+                form.remove()
+
+            }
+        })
+    }
+
 }
 
 
@@ -54,3 +135,8 @@ const card = new Card();
 //Eventos
 card.agregarNombre();
 card.agregarNumero();
+card.agregarFechaMM();
+card.agregarFechaYY();
+card.agregarCVC();
+card.formEnviado();
+
